d54fc71bc0d662292cea6ad0a14d717d1b8dd068
Harshit Choudhary <harshit2005stu@gmail.com>
2026-07-08 14:39:23 +0530

    Use environment variable for backend API URL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -4,9 +4,10 @@ import axios from "axios";
 import qs from "qs";
 console.log("ENV:", import.meta.env.VITE_API_URL);
 const api = axios.create({
-  baseURL: "http://localhost:5500/api",
+  baseURL: `${import.meta.env.VITE_API_URL || "http://localhost:5500"}/api`,
   withCredentials: true,
-  paramsSerializer: (params) => qs.stringify(params, { arrayFormat: "repeat" }),
+  paramsSerializer: (params) =>
+    qs.stringify(params, { arrayFormat: "repeat" }),
 });
 
 export default api;
